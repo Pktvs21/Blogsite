6600a507afd51a0da25ce96759928f7578ce90b8
Pavan Kumar .T <tvspk100@gmail.com>
2021-10-04 10:59:00 +0530

    Link Pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
-    return (
-        <div>
-           Navbar 
-        </div>
-    )
+  return (
+    <div>
+      <ul id="nav">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/create-post">CreatePost</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
