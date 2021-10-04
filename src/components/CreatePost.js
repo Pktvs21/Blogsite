2aa9b462902c3c4fac8a476858bf39e351155989
Pavan Kumar .T <tvspk100@gmail.com>
2021-10-04 11:18:03 +0530

    React-hooks state

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import {React,useState} from 'react'
 
 export default function CreatePost() {
+    const [title,setTitle] =useState();
+    const [content,setContent] =useState();
+    const [subTitle,setSubTitle] =useState();
+
+    function handleSubmit (e){
+        e.preventDefault();
+        console.log("title",title);
+        console.log(subTitle);
+        console.log(content);
+    }
     return (
-        <div>
-            CreatePost
+        <div className="create-post">
+            <h1>Create Post</h1>
+            <form onSubmit={handleSubmit}>
+                <div className="form-field">
+                    <label>Title</label>
+                    <input value={title} onChange={(e) =>setTitle(e.target.value)}/>
+                </div>
+                <div className="form-field">
+                    <label>Sub Title</label>
+                    <input value={subTitle} onChange={(e) =>setSubTitle(e.target.value)}/>
+                </div>
+                <div className="form-field">
+                    <label>Content</label>
+                    <textarea value={content} onChange={(e) =>setContent(e.target.value)}/>
+                </div>
+                <button className="create-post-btn">Create Post</button>
+            </form>
         </div>
     )
 }
