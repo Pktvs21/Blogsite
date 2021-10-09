8c786b70b503dd71960b4555b2fb49829fee70f9
Pavan Kumar .T <tvspk100@gmail.com>
2021-10-09 12:42:12 +0530

    Using Styled-components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { firestore } from '../firebase';
+import styled from 'styled-components';
+
+const Blogheading = styled.h1`
+  text-align: center;
+  color: #2196f3;
+  margin-bottom: 2px;
+  color:red;
+`;
+const SubTitle=styled.p`
+{font-size: 33px;}
+`;
 
 export default function Home() {
   const [posts, setPosts] = useState([]);
@@ -23,7 +34,7 @@ export default function Home() {
 
   return (
     <div className="home">
-      <h1>Tech Blog</h1>
+      <Blogheading>Tech Blog</Blogheading>
       <div id="blog-by">Name</div>
       {posts.map((post, index) => (
         <div className="post" key={`post-${index}`}>
@@ -31,7 +42,7 @@ export default function Home() {
             <h3>{post.title}</h3>
           </Link>
 
-          <p>{post.subTitle}</p>
+          <SubTitle>{post.subTitle}</SubTitle>
         </div>
       ))}
     </div>
