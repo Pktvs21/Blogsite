4ba8822ee85c51ca4de21aa015108a59991c0909
Pavan Kumar .T <tvspk100@gmail.com>
2021-10-10 12:40:06 +0530

    Using Radium for styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,23 @@
-import {Switch,Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import CreatePost from './CreatePost';
 import Home from './Home';
 import Navbar from './Navbar';
 import PostDetail from './PostDetail';
+import { StyleRoot } from 'radium';
 
 function App() {
-  return <div className="container">
-    <Navbar />
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/post/:postId' component={PostDetail}/>
-      <Route exact path='/create-post' component={CreatePost}/>
-    </Switch>
-  </div>;
+  return (
+    <StyleRoot>
+      <div className="container">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/post/:postId" component={PostDetail} />
+          <Route exact path="/create-post" component={CreatePost} />
+        </Switch>
+      </div>
+    </StyleRoot>
+  );
 }
 
 export default App;
--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { firestore } from '../firebase';
 import {useParams} from 'react-router-dom';
+import Radium from 'radium';
 
-export default function PostDetail() {
+function PostDetail() {
     const [post,setPost] =useState({});
     const {postId} =useParams();
     useEffect(()=>{
@@ -13,8 +14,28 @@ export default function PostDetail() {
     },[])
     return (
         <div className="post-detail">
-            <h1>{post.title}</h1>
-            <p>{post.content}</p>
+            <h1 style={styles.heading}>{post.title}</h1>
+            <p style={styles.PostDetail}>{post.content}</p>
         </div>
     )
+}
+export default Radium(PostDetail);
+
+
+const styles={
+    PostDetail:{
+        border:'1px solid #e1e1e1',
+        padding:5,
+        paddingTop:10,
+        borderRadius:5,
+        '@media(max-width:720px)':{
+            color:'pink'
+        }
+    },
+    heading:{
+        textAlign:'center',
+        ':hover':{
+            color:'red'
+        },
+    }
 }
